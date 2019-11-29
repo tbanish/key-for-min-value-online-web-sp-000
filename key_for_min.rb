4adadaf6175f01d44ab74204053040692fda1eb7
Tom Banish <tombanish@gmail.com>
2019-11-29 02:25:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-# name_hash = {:blake => 500, :ashley => 2, :adam => 1}
-
 
 def key_for_min_value(name_hash)
   if name_hash == {}
