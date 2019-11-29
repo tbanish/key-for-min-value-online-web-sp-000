21bd354bb640e7951c12f7995a32bd13ca962ef2
Tom Banish <tombanish@gmail.com>
2019-11-29 01:58:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,11 @@
 
 
 def key_for_min_value(name_hash)
+  min_k = name_hash.first[0]
+  min_v = name_hash.first[1]
   if name_hash = {}
     nil
   else
     name_hash.each do |key, value|
-    
+      if 
 end
